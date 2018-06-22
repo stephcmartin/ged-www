8c91dac06209c1dd34a1626e407696c189e7caa3
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-22 16:25:09 -0400

    Added logic to render success, skip and error object

--- index.js
+++ index.js
@@ -6,28 +6,50 @@ fs.readFile('./src/internet1.json', handleFile)
 
 // Write the callback function
 function handleFile(err, data) {
-    if (err) throw err
-    let obj = JSON.parse(data)
+  if (err) throw err
+  let internet = JSON.parse(data)
 
-    // obj is now the entire json object
-    // First: Write a function that lists obj.pages[i].address as 'success'
+  // internet is now the entire json object
 
-    let results = {
+// Initialize empty object to hold data
+  let results = {
     "success": [],
     "skipped": [],
     "error": []
-    }
+  }
 
-    const success = obj.pages.map((link)=> {
-        return results.success.push(link.address)
+  let crawls =[]
+
+  // First: Write a function that lists obj.pages[i].address as 'success'
+
+  const succesfulCrawls = internet.pages.map((address) => {
+    results.success.push(address.address)
+  })
+
+  // this outputs:
+  // [ 'http://foo.bar.com/p1', 'http://foo.bar.com/p2', 'http://foo.bar.com/p4',
+  // 'http://foo.bar.com/p5','http://foo.bar.com/p6' ]
+
+  // Second: Write a function that will check if it needs to be skipped.
+  // Remember: if the address is already in success array, we add it to skip.
+  // i.e what overlaps between internet.pages.address and internet.pages.links?
+
+  const checkAddress = internet.pages.map((address) => {
+    // console.log('address.links', address.links)
+    // console.log('results.success.includes(address)', results.success.includes(address.links))
+
+    const mappingThrough = address.links.map((item)=> {
+      console.log(item)
+      if (results.success.includes(item)){
+        results.skipped.push(item)
+      } else {
+        // The items that don't exist get pushed to the errors array
+        results.error.push(item)
+      }
     })
-      console.log(results)
-      // Yay! this outputs:
-      // [ 'http://foo.bar.com/p1', 'http://foo.bar.com/p2', 'http://foo.bar.com/p4',
-      // 'http://foo.bar.com/p5','http://foo.bar.com/p6' ]
+})
 
-      // Second: Write a function that will check if it needs to be skipped.
-      // Remember: if address is already in success array, we skip.
+console.log(results)
 
 
 }
