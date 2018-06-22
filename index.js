064b58d6f71b5904b20e75c93fed3cdda8d8a141
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-22 16:27:55 -0400

    Tidied up explaination

--- index.js
+++ index.js
@@ -11,42 +11,35 @@ function handleFile(err, data) {
 
   // internet is now the entire json object
 
-// Initialize empty object to hold data
+  // Initialize empty object to hold data
   let results = {
     "success": [],
     "skipped": [],
     "error": []
   }
 
-  // First: Write a function that lists obj.pages[i].address as 'success'
+  // Write a function that lists obj.pages[i].address as 'success'
 
   const succesfulCrawls = internet.pages.map((address) => {
     results.success.push(address.address)
   })
 
-  // this outputs:
-  // [ 'http://foo.bar.com/p1', 'http://foo.bar.com/p2', 'http://foo.bar.com/p4',
-  // 'http://foo.bar.com/p5','http://foo.bar.com/p6' ]
-
-  // Second: Write a function that will check if it needs to be skipped.
+  // Write a function that will check if it needs to be skipped.
   // Remember: if the address is already in success array, we add it to skip.
-  // i.e what overlaps between internet.pages.address and internet.pages.links?
 
   const checkAddress = internet.pages.map((address) => {
-    // console.log('address.links', address.links)
-    // console.log('results.success.includes(address)', results.success.includes(address.links))
-
-    const mappingThrough = address.links.map((item)=> {
-      if (results.success.includes(item)){
+    // This function maps through all the internet.pages.links
+    const mappingThrough = address.links.map((item) => {
+      if (results.success.includes(item)) {
+        // if it already exists, push it to the skipped array
         results.skipped.push(item)
       } else {
         // The items that don't exist get pushed to the errors array
         results.error.push(item)
       }
     })
-})
+  })
 
-console.log(results)
-
+  console.log(results)
 
 }
